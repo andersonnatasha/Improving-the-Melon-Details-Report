1768031c58cadd22869f257e0400db5aa13a12d6
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-06 05:03:45 +0000

    initial commit

---
+++ melons_edited.py
@@ -0,0 +1,42 @@
+melons = {
+    'Honeydew': {
+        'price': .99,
+        'seedless': True, 
+        'fless_color': 'Unknown',
+        'rind_color': 'Unknown',
+        'weight': 'Unknown',
+        'location_grown': 'Unknown'
+    },
+    'Crenshaw': {
+        'price': 2.00,
+        'seedless': False,
+        'fless_color': 'Unknown',
+        'rind_color': 'Unknown',
+        'weight': 'Unknown',
+        'location_grown': 'Unknown'
+    },
+    'Crane':{
+        'price': 2.50,
+        'seedless': False,
+        'fless_color': 'Unknown',
+        'rind_color': 'Unknown',
+        'weight': 'Unknown',
+        'location_grown': 'Unknown'
+    },
+    'Casaba': {
+        'price': 2.50,
+        'seedless': False,
+        'fless_color': 'Unknown',
+        'rind_color': 'Unknown',
+        'weight': 'Unknown',
+        'location_grown': 'Unknown'
+    },
+    "Cantaloupe": {
+        'price': .99,
+        'seedles': False,
+        'fless_color': 'Unknown',
+        'rind_color': 'Unknown',
+        'weight': 'Unknown',
+        'location_grown': 'Unknown'
+    }
+}
